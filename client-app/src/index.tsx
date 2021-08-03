61e734f651caa955904df024a41bd81993cc72a5
Lucas N. Coelho <lucascooelho@hotmail.com>
2021-08-03 23:22:11 +0100

    Mobx Added/Updated CRUD MobX/js map obj to store.

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -4,12 +4,15 @@ import 'semantic-ui-css/semantic.min.css'
 import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
+import { store, StoreContext } from './app/stores/store';
 
 ReactDOM.render(
   // StrictMode does not allow deprecated methods.
   //<React.StrictMode>
   //</React.StrictMode>,
-  <App />,
+  <StoreContext.Provider value={store}>
+    <App />
+  </StoreContext.Provider>,
   document.getElementById('root')
 );
 
