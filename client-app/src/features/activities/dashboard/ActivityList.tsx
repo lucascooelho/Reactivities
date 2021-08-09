e1cc66a00209f126edb60bb8cdf3cf89f6cdeb28
Lucas N. Coelho <lucascooelho@hotmail.com>
2021-08-09 23:46:39 +0100

    Added React Routes/Nav Link/Route Params

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -3,6 +3,7 @@ import { observer } from "mobx-react-lite";
 import { useState, SyntheticEvent } from "react";
 import { Button, Item, Label, Segment } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
+import { Link } from "react-router-dom";
 
 export default observer(function ActivityList() {
   const {activityStore} = useStore();
@@ -14,8 +15,6 @@ export default observer(function ActivityList() {
     setTarget(e.currentTarget.name);
     deleteActivity(id);
   }
-
-
 
   return (
     <Segment>
@@ -30,7 +29,7 @@ export default observer(function ActivityList() {
                 <div>{activity.city}, {activity.venue}</div>
               </Item.Description>
               <Item.Extra>
-                <Button onClick={() => activityStore.selectActivity(activity.id)} floated="right" content="View" color="blue" />
+                <Button as={Link} to={`/activities/${activity.id}`} floated="right" content="View" color="blue" />
                 <Button
                   name={activity.id}
                   loading={loading && target === activity.id}
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -5,13 +5,16 @@ import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   // StrictMode does not allow deprecated methods.
   //<React.StrictMode>
   //</React.StrictMode>,
   <StoreContext.Provider value={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StoreContext.Provider>,
   document.getElementById('root')
 );
