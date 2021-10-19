ad26bdb8ccc70637d4b62cd780dfa011022e265d
Lucas N. Coelho <lucascooelho@hotmail.com>
2021-10-20 00:42:45 +0100

    Login Form/Display Error/Token Login/Modal

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -3,6 +3,7 @@ import { Activity } from './../models/activity';
 import axios, { AxiosError, AxiosResponse } from 'axios';
 import { toast } from 'react-toastify';
 import { history } from '../..';
+import { User, UserFormValues } from '../models/user';
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -11,6 +12,14 @@ const sleep = (delay: number) => {
 }
 
 axios.defaults.baseURL = 'http://localhost:5000/api';
+
+axios.interceptors.request.use(config => {
+    const token = store.commonStore.token;
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
+    return config;
+})
 
 axios.interceptors.response.use(async response => {
     await sleep(1000);
@@ -68,8 +77,15 @@ const Activities = {
     delete: (id: string) => axios.delete<void>(`/Activities/${id}`),
 };
 
+const Account = {
+    current: () => request.get<User>('/account'),
+    login: (user: UserFormValues) => request.post<User>('/account/login', user),
+    register: (user: UserFormValues) => request.post<User>('/account/register', user)
+};
+
 const agent = {
-    Activities
+    Activities,
+    Account
 };
 
 export default agent;
