a6fa4f4759178a7116db9cfc76053143245e01b5
Lucas N. Coelho <lucascooelho@hotmail.com>
2021-11-02 00:33:03 +0000

    EF Relations/Load Related Entity/AutoMapper Querys

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+using Application.Activities;
 using AutoMapper;
 using Domain;
 
@@ -8,6 +10,15 @@ namespace Application.Core
         public MappingProfiles()
         {
             CreateMap<Activity, Activity>();
+            CreateMap<Activity, ActivityDto>()
+                .ForMember(destination => destination.HostUsername, options =>
+                    options.MapFrom(s => s.Attendees.FirstOrDefault(x => x.IsHost).AppUser.UserName));
+
+            CreateMap<ActivityAttendee, Profiles.Profile>()
+                .ForMember(destination => destination.DisplayName, options => options.MapFrom(s => s.AppUser.DisplayName))
+                .ForMember(destination => destination.Username, options => options.MapFrom(s => s.AppUser.UserName))
+                .ForMember(destination => destination.Bio, options => options.MapFrom(s => s.AppUser.Bio));
+
         }
     }
 }
